bae01ac22b5f4fd8f6277eedffe5ba9d3d3282b5
Mahesh Jadhav <omkar28399@gmail.com>
2024-02-03 21:49:26 +0530

    Added CustomPermissions

--- api9/views.py
+++ api9/views.py
@@ -3,11 +3,14 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+
 from .serializers import StudentSerializer
 from api.models import Student
+from .custompermissions import MyPermission
 
 class StudentAPI9(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
