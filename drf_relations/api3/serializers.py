5978ede29ba8823802f536b258586afb7be55c21
Mahesh Jadhav <omkar28399@gmail.com>
2024-02-16 18:41:47 +0530

    Added Netsted Serializers in api3

---
+++ drf_relations/api3/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from api.serializers import Singer,Song
+
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = '__all__'
+
+class SingerSerializer(serializers.ModelSerializer):
+    song = SongSerializer(many=True,read_only=True)
+    class Meta:
+        model = Singer
+        fields = '__all__'
