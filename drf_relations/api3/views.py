da7f0ebe5a6e95f0aca04ce13819c6a0b8590be1
Mahesh Jadhav <omkar28399@gmail.com>
2024-02-16 18:41:47 +0530

    Added Netsted Serializers in api3

--- drf_relations/api3/views.py
+++ drf_relations/api3/views.py
@@ -2,13 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 
 # from .models import Singer,Song
 from .serializers import SingerSerializer,SongSerializer
-from .models import Singer,Song
+from api.models import Singer,Song
 
 # Create your views here.
-class SingerAPI(ModelViewSet):
+class SingerAPI3(ModelViewSet):
     queryset = Singer.objects.all()
     serializer_class = SingerSerializer
 
-class SongAPI(ModelViewSet):
+class SongAPI3(ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
--- drf_relations/drf_relations/urls.py
+++ drf_relations/drf_relations/urls.py
@@ -20,12 +20,15 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import SingerAPI,SongAPI
 from api2.views import SingerAPI2,SongAPI2
+from api3.views import SingerAPI3,SongAPI3
 
 router = DefaultRouter()
 router.register('singerapi',SingerAPI)
 router.register('songapi',SongAPI)
 router.register('singerapi2',SingerAPI2)
 router.register('songapi2',SongAPI2)
+router.register('singerapi3',SingerAPI3)
+router.register('songapi3',SongAPI3)
 
 
 urlpatterns = [
