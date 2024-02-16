49b3be00ff7ddccfcdbf7040456ab20d2311723d
Mahesh Jadhav <omkar28399@gmail.com>
2024-02-16 14:06:58 +0530

    Added all relation fields in serializers.py

--- drf_relations/api/serializers.py
+++ drf_relations/api/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from .models import Singer,Song
 
 class SingerSerializer(serializers.ModelSerializer):
-    song = serializers.StringRelatedField(many=True,read_only=True)
+    # song = serializers.StringRelatedField(many=True,read_only=True)
+    # song = serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+    # song = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='song-detail')
+    # song = serializers.SlugRelatedField(many=True,read_only=True,slug_field='title')
+    # song = serializers.HyperlinkedIdentityField(view_name='song-detail')   ###Same like HyperlinkedRelatedField but needs to work with hyperlinked model serializer
+
     class Meta:
         model = Singer
         fields = ['id','name','gender','song']
